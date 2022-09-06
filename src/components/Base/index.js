edbfb48a23a27913a251066938d3a9de9779472e
pristtit <pristtit@mail.ru>
2022-09-06 22:11:12 +0300

    удаление остатков предыдущих реализаций

--- src/components/Base/index.js
+++ src/components/Base/index.js
@@ -1,20 +1,12 @@
 import BaseInput from "@/components/Base/BaseInput";
-import BaseModal from "@/components/Base/BaseModal";
-import BasePostForm from "@/components/Base/BasePostForm";
 import BaseButton from "@/components/Base/BaseButton";
-import BaseLogin from "@/components/Base/BaseLogin";
-import BaseSelect from "@/components/Base/BaseSelect";
 import BaseError from "@/components/Base/BaseError";
 import BaseCastomSelect from "@/components/Base/BaseCastomSelect";
 
 
 export default [
     BaseInput,
-    BaseModal,
-    BasePostForm,
     BaseButton,
-    BaseLogin,
-    BaseSelect,
     BaseError,
     BaseCastomSelect,
 ];
